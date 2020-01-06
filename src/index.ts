b4802f15cbc83f8ef19ade6b08a99e539ed6eea0
Andreas <andreas.groos1@gmail.com>
2020-01-05 22:03:51 -0800

    Some basic function like isPoint(), getCoords() etc

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,41 @@
-export const Greeter = (name: string) => {
-    console.log(name)
-    return `Hello ${name}`;
+import { GeoJSON, GeoJsonProperties, Geometry, Feature, LineString, Position, Polygon, Point } from 'geojson'
+import river from './river'
+
+export const geojsonType = (json: GeoJSON): string => {
+    return json.type
 }
 
-Greeter('Andreas')
+export const isPoint = (json: GeoJSON): boolean => {
+    return json.type === 'Point'
+}
+
+export const isPolygon = (json: GeoJSON): boolean => {
+    return json.type === 'Polygon'
+}
+
+export const isFeatureCollection = (json: GeoJSON): boolean => {
+    return json.type === 'FeatureCollection'
+}
+
+export const isLineString = (json: Feature<Geometry, any>): boolean => {
+    return json.geometry.type === 'LineString'
+}
+
+export const getFeatures = (json: GeoJSON): Feature<Geometry, GeoJsonProperties>[] | [] => {
+    if (json.type === 'FeatureCollection') {
+        return json.features
+    }
+    return []
+}
+
+export const getCoords = (json: Feature<Point | LineString | Polygon, GeoJsonProperties>): Position | Position[] | Position[][] => {
+    return json.geometry.coordinates
+}
+
+// console.log('river', river)
+const r = river as Feature<LineString, any>
+if (isLineString(r)) {
+    // if (isPoint(r)) {
+    const c = getCoords(r)
+    console.log('c', c)
+}
